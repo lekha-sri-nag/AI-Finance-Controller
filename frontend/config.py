f9782ff8bd795c88bc2d2ca0d83b57400a03c196
Vutukuri Lekha Sri Nag <lekhasrinag@gmail.com>
2026-09-08 21:03:48 +0000

    Connect frontend to Streamlit secrets

--- frontend/config.py
+++ frontend/config.py
@@ -1,3 +1,7 @@
 import os
+import streamlit as st
 
-API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
+try:
+    API_URL = st.secrets.get("API_URL", os.getenv("API_URL", "http://127.0.0.1:8000"))
+except Exception:
+    API_URL = os.getenv("API_URL", "http://127.0.0.1:8000")
